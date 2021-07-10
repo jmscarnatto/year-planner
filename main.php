08a433a913e4217430ff9ee2a57c5060db7bb7fa
Jose Scarnatto <jmscarnatto@gmail.com>
2021-07-10 19:43:35 -0300

    Update main.php

--- main.php
+++ main.php
@@ -2,95 +2,107 @@
 
 date_default_timezone_set('America/Sao_Paulo');  
 
-$aux = $_GET["y"]; 
+$requested_year = $_GET["y"]; 
 
-if (isset($aux)) 
+if (isset($requested_year)) 
 { 
-  $g6ahs = $aux; 
+  $current_year = $requested_year; 
 } else 
 { 
-  $g6ahs = date("Y");
+  $current_year = date("Y");
 }  
 
 $thisDay = date("d");
-$thisi98s = date("m");
+$thisMonth = date("m");
 $feriados = array();
 
-holydays(); 
+holidays(); 
 $fds = array(1,7,8,14,15,21,22,28,29,35,36);
 $dia = array("S","M","T","W","T","F","S");
-$i98s = array("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec");
-$mes = array(31,fDays($g6ahs),31,30,31,30,31,31,30,31,30,31); 
-$uda32 = array(dS(1),dS(2),dS(3),dS(4),dS(5),dS(6),dS(7),dS(8),dS(9),dS(10),dS(11),dS(12));  
-$gfd = "<div id='globalYearPlanner'><ul><li style='width: 35px;'>&nbsp;</li>";  
+$months_names = array("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec");
+$days_in_month = array(31,february_days($current_year),31,30,31,30,31,31,30,31,30,31); 
+$first_day_number_of_month = array(
+                days_on_month(1),
+                days_on_month(2),
+                days_on_month(3),
+                days_on_month(4),
+                days_on_month(5),
+                days_on_month(6),
+                days_on_month(7),
+                days_on_month(8),
+                days_on_month(9),
+                days_on_month(10),
+                days_on_month(11),
+                days_on_month(12)
+                );  
+$HTML = "<div id='globalYearPlanner'><ul><li style='width: 35px;'>&nbsp;</li>";  
 
 for ($z=1; $z<=5;$z++)
 { 
     for ($x=0; $x<=6; $x++)
     {  
-        $gfd .= "<li class='dias' >".$dia[$x]."</li>";
+        $HTML .= "<li class='dias' >".$dia[$x]."</li>";
     } 
 }  
 
-$gfd .= "<li class='dias'>S</li><li class='dias'>M</li></ul>";
+$HTML .= "<li class='dias'>S</li><li class='dias'>M</li></ul>";
 $numDay = 1; 
 for ($y=0; $y<=11;$y++)
 { 
-    $gfd.="<ul><li width=17 align='center' class='meses'>".$i98s[$y]."</li>"; 
+    $HTML.="<ul><li width=17 align='center' class='meses'>".$months_names[$y]."</li>"; 
     for ($w=1; $w<=37;$w++)
     {
-        if ($w<$uda32[$y] || $w>($mes[$y]-1 + $uda32[$y])) 
+        if ($w<$first_day_number_of_month[$y] || $w>($days_in_month[$y]-1 + $first_day_number_of_month[$y])) 
         { 
-            $gfd.="<li class='vacios'></li>"; 
+            $HTML.="<li class='vacios'></li>"; 
         } else 
         { 
-            $r543y = $w + 1 - $uda32[$y];  
-            $thisNewDay = "#".($y + 1)."/".$r543y."/".$g6ahs."#"; 
-            $thisDayName = jddayofweek(cal_to_jd(CAL_GREGORIAN,($y + 1),$r543y,$g6ahs),2); 
+            $r543y = $w + 1 - $first_day_number_of_month[$y];  
+            $thisNewDay = "#".($y + 1)."/".$r543y."/".$current_year."#"; 
+            $thisDayName = jddayofweek(cal_to_jd(CAL_GREGORIAN,($y + 1),$r543y,$current_year),2); 
             if($r543y<10) 
             { 
-                $y76s= $i98s[$y]."0".$r543y; 
+                $y76s= $months_names[$y]."0".$r543y; 
             } else
             { 
-                $y76s= $i98s[$y].$r543y;
+                $y76s= $months_names[$y].$r543y;
             } 
-            $gfd.="<li id='".$y76s."' "; 
+            $HTML.="<li id='".$y76s."' "; 
             if($thisDayName=="Sat" || $thisDayName=="Sun")
             { 
-                $gfd.="name='".$thisDayName."' "; 
+                $HTML.="name='".$thisDayName."' "; 
             } else
             { 
-                $gfd.="name='".$numDay."' ";
+                $HTML.="name='".$numDay."' ";
             } 
-            $gfd.= "class='".Formatting($w,$y,$r543y).$r543y."</li>"; $numDay++;
+            $HTML.= "class='".Formatting($w,$y,$r543y).$r543y."</li>"; $numDay++;
         } 
     } 
-    $gfd .= "</ul>";
+    $HTML .= "</ul>";
 } 
-$gfd.="</div>";
-echo $gfd;
+$HTML.="</div>";
+echo $HTML;
 
-function fDays($someYear) 
+function february_days($some_year) 
 { 
-    if (checkdate(2,29,$someYear)) 
+    if (checkdate(2,29,$some_year)) 
     { 
-        $february_days = 29; 
+        return 29; 
     } else 
     { 
-        $february_days = 28; 
+        return 28; 
     } 
-    return $february_days;
  }  
 
-function dS($mesId) 
+function days_on_month($mesId) 
 { 
-    global $g6ahs; 
-    return(jddayofweek(cal_to_jd(CAL_GREGORIAN,$mesId,1,$g6ahs),0))+1; 
+    global $current_year; 
+    return(jddayofweek(cal_to_jd(CAL_GREGORIAN,$mesId,1,$current_year),0))+1; 
 }  
 
 function Formatting($nro_col, $nro_mes, $nro_dia) 
 {
-    global $fds, $thisDay, $thisi98s, $feriados; $Format = "normal'>"; 
+    global $fds, $thisDay, $thisMonth, $feriados; $Format = "normal'>"; 
     for ($i=0;$i<=10;$i++) 
     { 
         if ($nro_col == $fds[$i]) 
@@ -105,14 +117,14 @@ function Formatting($nro_col, $nro_mes, $nro_dia)
             $Format = "feriado'>";
         }          
     }
-    if ($nro_dia == $thisDay && $nro_mes == ($thisi98s-1)) 
+    if ($nro_dia == $thisDay && $nro_mes == ($thisMonth-1)) 
     { 
             $Format = "hoje'>";
     } 
     return $Format; 
 }  
 
-function holydays() 
+function holidays() 
 { 
     global $feriados; 
     $Doc = new DOMDocument(); 
@@ -127,4 +139,4 @@ function holydays()
     return $feriados; 
 }  
 
-?>  
+?> 
